85755299239d0de1fadaea91c6a472f4a11c94c9
Margarida Martelo <margmartelo@gmail.com>
2020-11-10 21:45:50 +0000

    Adding cat picture (and a few things more)

--- app.rb
+++ app.rb
@@ -1,5 +1,19 @@
 require 'sinatra'
 
 get '/' do
-  'Hello world!'
+  'hello!'
+end
+
+get '/secret' do
+  "This is secret content."
+end
+
+get '/test' do
+  "Just trying!"
+end
+
+get '/cat' do
+  "<div>
+    <img src='http://bit.ly/1eze8aE'>
+   </div>"
 end
