7d404e603b78f9950e3744dff42b303ec3cdb16d
Margarida Martelo <margmartelo@gmail.com>
2020-11-10 22:17:36 +0000

    Separation of Concerns

--- app.rb
+++ app.rb
@@ -1,19 +1,5 @@
 require 'sinatra'
 
-get '/' do
-  'hello!'
-end
-
-get '/secret' do
-  "This is secret content."
-end
-
-get '/test' do
-  "Just trying!"
-end
-
 get '/cat' do
-  "<div style='border: 3px dashed red'>
-    <img src='http://bit.ly/1eze8aE'>
-   </div>"
+  erb(:index)
 end
