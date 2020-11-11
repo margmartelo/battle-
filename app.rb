ef71f97b2bc674e1efd8ba29cf33311ec845c725
Margarida Martelo <margmartelo@gmail.com>
2020-11-11 19:21:13 +0000

    getting started with battle

--- app.rb
+++ app.rb
@@ -1,16 +1,9 @@
-require 'sinatra'
+require 'sinatra/base'
 
-get '/random-cat' do
-  @name = ["Amigo", "Oscar", "Viking"].sample
-  erb(:index)
-end
+class Battle < Sinatra::Base
+  get '/' do
+    'Hello Battle!'
+  end
 
-post '/named-cat' do
-  p params
-  @name = params[:name]
-  erb(:index)
-end
-
-get '/cat-form' do
-  erb(:cat_form)
+  run! if app_file == $0
 end
